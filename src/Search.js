2b6b5253a98afdfca1d09374c7225b90fd50b93c
blahahaha13 <gmony1558@gmail.com>
2018-11-08 14:27:21 -0800

    modified search to grab input values and console log them

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      query: ''
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    let query = this.state.query
+    console.log(query)
+  }
   render (){
     return (
       <div className="Searchbar">
-        <form>
-          <input placeholder='Search for Gifs' type='text' />
+        <form onSubmit={ this.onSubmit }>
+          <input 
+            placeholder='Search for Gifs'
+            type='text'
+            onChange={ this.onChange }
+            value={this.state.query} />
           <button type="submit">Search</button> 
         </form>
       </div>
